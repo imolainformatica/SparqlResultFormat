462f69386402ac6c430f6671c9a3adf0af977901
gcornacchia <gcornacchia@imolinfo.it>
2019-04-16 00:20:25 +0200

    fixate segnalazioni eslint

--- js/ii.sparql.start.js
+++ js/ii.sparql.start.js
@@ -1,7 +1,10 @@
-if ( window.sparqlResultFormatsElements ) {
-	for ( var i = 0; i < window.sparqlResultFormatsElements.length; i++ ) {
-		var el = window.sparqlResultFormatsElements[ i ],
-		 conf = el.config;
-		el.start( conf );
+( function () {
+	var el, conf, i;
+	if ( window.sparqlResultFormatsElements ) {
+		for ( i = 0; i < window.sparqlResultFormatsElements.length; i++ ) {
+			el = window.sparqlResultFormatsElements[ i ];
+			conf = el.config;
+			el.start( conf );
+		}
 	}
-}
+}() );
